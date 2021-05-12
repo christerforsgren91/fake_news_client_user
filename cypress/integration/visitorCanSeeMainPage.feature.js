fcdf5f7f67caec3b908304547f4990fe5cb9f848
kim_haaga <kim.haaga@live.se>
2021-05-12 14:48:11 +0200

    Add test for mainpage layout

---
+++ cypress/integration/visitorCanSeeMainPage.feature.js
@@ -0,0 +1,14 @@
+describe('Visitor can see main page', () => {
+  describe('Success', () => {
+    beforeEach(() =>{
+      cy.visit('/')
+    })
+    it('displays main page layout', () => {
+      cy.get('[data-cy=navbar]').should('exist')
+      cy.get('[data-cy=header]').should('exist')
+      cy.get('[data-cy=category-container]').children().should('have.length', 5)
+      cy.get('[data-cy=articles-container]').should('exist')
+      cy.get('[data-cy=footer]').should('exist')
+    })
+  })
+})
