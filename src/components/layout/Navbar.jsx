dfee4fc8682b915f3a2e9f4fb2b825cc76a5d8f4
DavDan1 <david-danielyan@hotmail.com>
2021-05-12 16:00:41 +0200

    add navbar add navigation buttons

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Segment, Header } from 'semantic-ui-react';
+import { Segment, Tab } from 'semantic-ui-react';
 
 const Navbar = () => {
+  const panes = [
+    {
+      menuItem: 'Home',
+    },
+    {
+      menuItem: 'About',
+    },
+    {
+      menuItem: 'Search',
+    },
+    {
+      menuItem: 'Log In',
+    },
+    {
+      menuItem: 'Language',
+    },
+  ];
   return (
     <>
-      <Segment id='navbar' fluid inverted size='massive' color='red'>
-      
+      <Segment id='navbar' fluid inverted size='massive'>
+        <Tab
+          menu={{ secondary: true, pointing: true, fluid: true }}
+          panes={panes}
+        />
       </Segment>
     </>
   );
