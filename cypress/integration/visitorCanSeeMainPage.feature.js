1736f0658516b0f5e7829dc9a5f5edcfb3538058
kim_haaga <kim.haaga@live.se>
2021-05-13 15:43:49 +0200

    Update test

--- cypress/integration/visitorCanSeeMainPage.feature.js
+++ cypress/integration/visitorCanSeeMainPage.feature.js
@@ -10,9 +10,11 @@ describe('Visitor can see main page', () => {
       cy.get('[data-cy=navbar]').should('exist');
       cy.get('[data-cy=header]').should('exist');
       cy.get('[data-cy=breaking-news]').should('exist');
-      cy.get('[data-cy=category-container]')
-        .children()
-        .should('have.length', 5);
+      cy.get('[data-cy=braking-img]').should('exist');
+      cy.get('[data-cy=braking-img]').should('exist');
+      cy.get('[data-cy=breaking-title]').should('contain', 'Something');
+      cy.get('[data-cy=breaking-teaser]').should('contain', 'Something more');
+      cy.get('[data-cy=breaking-date]').should('contain', '2021-05-12');
       cy.get('[data-cy=articles-container]').should('exist');
       cy.get('[data-cy=footer]').should('exist');
     });
--- src/components/layout/BreakingNews.jsx
+++ src/components/layout/BreakingNews.jsx
@@ -6,14 +6,15 @@ const BreakingNews = ({ firstArticle }) => {
       <div>
         <div className='breaking-news' data-cy='breaking-news' placeholder>
           <img
+          data-cy='braking-img'
             className='braking-img'
             src='https://images.unsplash.com/photo-1487758608033-7780b34680ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3589&q=80'
             alt=''
           />
           <div className='breaking-content'>
-            <h2>{firstArticle.title}</h2>
-            <h4>{firstArticle.teaser}</h4>
-            <p>{firstArticle.date}</p>
+            <h2 data-cy='breaking-title' >{firstArticle.title}</h2>
+            <h4 data-cy='breaking-teaser' >{firstArticle.teaser}</h4>
+            <p data-cy='breaking-date' >{firstArticle.date}</p>
           </div>
         </div>
       </div>
