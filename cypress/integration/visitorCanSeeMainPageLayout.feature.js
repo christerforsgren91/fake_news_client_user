3b59d93a7a7407af03f4f69a7bb6a752d578c8ff
DavDan1 <david-danielyan@hotmail.com>
2021-05-14 12:19:21 +0200

    Update test for main page layout from exist to contain

--- cypress/integration/visitorCanSeeMainPageLayout.feature.js
+++ cypress/integration/visitorCanSeeMainPageLayout.feature.js
@@ -7,23 +7,29 @@ describe('Visitor can see main page', () => {
       cy.visit('/');
     });
     it('displays main page layout', () => {
-      cy.get('[data-cy=navbar]').should('exist');
-      cy.get('[data-cy=header]').should('exist');
-      cy.get('[data-cy=breaking-news]').should('exist');
-      cy.get('[data-cy=breaking-img]').should('exist');
+      cy.get('[data-cy=navbar]').should('contain', 'Home');
+      cy.get('[data-cy=header]').should('contain', []);
+      cy.get('[data-cy=breaking-news]').should('contain', [0]);
       cy.get('[data-cy=breaking-img]').should('exist');
       cy.get('[data-cy=breaking-title]').should('contain', 'Something');
       cy.get('[data-cy=breaking-teaser]').should('contain', 'Something more');
       cy.get('[data-cy=breaking-date]').should('contain', '2021-05-12');
-      cy.get('[data-cy=articles-container]').should('exist');
+      cy.get('[data-cy=articles-container]').should('contain', []);
       cy.get('[data-cy=footer]').should('exist');
+      cy.get('[data-cy=articles-container]').within(() => {
+        cy.get('[data-cy=article-card-0]').within(() => {
+          cy.get('[data-cy=title]').should('contain', 'Sup');
+          cy.get('[data-cy=teaser]').should('contain', 'something');
+          cy.get('[data-cy=created-at]').should('contain', '2021-05-12');
+        });
+      });
     });
   });
   describe('unsuccessfully', () => {
     before(() => {
       cy.intercept('GET', 'https://fake-newzzzz.herokuapp.com/api/articles', {
         statusCode: 500,
-        error: '500 Internal Server Error |  0     bytes\n',
+        error: '500 Internal Server Error',
       });
       cy.visit('/');
     });
