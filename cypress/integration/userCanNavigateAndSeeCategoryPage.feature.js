0bef11a7967bdedbf6520189acbced727a3b1b96
arunbhalli <tomarbhalli@gmail.com>
2021-05-16 14:46:47 +0200

    Add sad path for navigation bar Refactores test

--- cypress/integration/userCanNavigateAndSeeCategoryPage.feature.js
+++ cypress/integration/userCanNavigateAndSeeCategoryPage.feature.js
@@ -3,21 +3,41 @@ describe('User is able to navigate to Category page', () => {
     cy.intercept('GET', 'https://fake-newzzzzz.herokuapp.com/api/articles/', {
       fixture: 'articles.json',
     });
-
-    cy.intercept('GET', 'https://fake-newzzzzz.herokuapp.com/api/articles/**', {
-      fixture: 'categories.json',
-    });
-
     cy.visit('/');
   });
-
-  it('user is able to navigate to category page', () => {
-    cy.get('[data-cy=category-bar]').within(() => {
-      cy.get('[data-cy=category-button]').should('have.length', 6);
-      cy.get('[data-cy=category-button]').eq(4).click();
+  describe('Successfully', () => {
+    beforeEach(() => {
+      cy.intercept(
+        'GET',
+        'https://fake-newzzzzz.herokuapp.com/api/articles/**',
+        {
+          fixture: 'categories.json',
+        }
+      );
     });
+
+    it('user is able to navigate to category page', () => {
+      cy.get('[data-cy=category-bar]').within(() => {
+        cy.get('[data-cy=category-button]').should('have.length', 6);
+        cy.get('[data-cy=category-button]').eq(3).click();
+      });
       cy.get('[data-cy=category-header]').should('contain', 'Politics');
       cy.url().should('contain', 'http://localhost:3001/category');
-    
+    });
+  });
+
+  describe('unsuccessfully There is no article in this category', () => {
+    beforeEach(() => {
+      cy.intercept(
+        'GET',
+        'https://fake-newzzzzz.herokuapp.com/api/articles/**',
+        {
+          articles: [],
+        }
+      );
+    });
+    it ('is expected to display error message', () => {
+      cy.get('[data-cy=error-message]').should('contain', 'Selected category does not have any articles');
+    })
   });
 });
