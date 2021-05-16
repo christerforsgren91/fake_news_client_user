0eccb7be46af4596db9316e987d6caa7906497be
Christer <christerforsgren91@gmail.com>
2021-05-16 16:50:10 +0200

    Add test so we get right amount of articles

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -5,7 +5,7 @@ const ArticleCard = ({ article, index }) => {
   return (
     <div className='card-container' data-cy={`article-card-${index}`}>
       <Link to={`/articles/${article.id}`}>
-        <div id={article.id}>
+        <div id={article.id} data-cy='article' >
           <img
             className='card-image'
             src='https://images.unsplash.com/photo-1487758608033-7780b34680ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3589&q=80'
