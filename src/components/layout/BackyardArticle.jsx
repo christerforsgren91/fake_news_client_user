7282831dc29b5744f38a003619d7103c0f07f86a
kim_haaga <kim.haaga@live.se>
2021-05-22 23:53:57 +0200

    Add backyard article style

--- src/components/layout/BackyardArticle.jsx
+++ src/components/layout/BackyardArticle.jsx
@@ -1,37 +1,36 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import BackyardArticles from '../../modules/BackyardArticles'
-import { useParams } from 'react-router'
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import BackyardArticles from '../../modules/BackyardArticles';
+import { useParams } from 'react-router';
 
 const BackyardArticle = () => {
-  const { backyardArticle } = useSelector(state => state)
+  const { backyardArticle } = useSelector((state) => state);
   const { id } = useParams();
 
-
   useEffect(() => {
-    BackyardArticles.show(id)
-  },[])
+    BackyardArticles.show(id);
+    // eslint-disable-next-line
+  }, []);
 
   return (
-    <div data-cy='article-container' style={styles.container} >
-      <h1 data-cy='title' >{backyardArticle.title}</h1>
-      <p data-cy='theme' >{backyardArticle.theme}</p>
-      <p data-cy='date' >{backyardArticle.date}</p>
-      <p data-cy='author' >{backyardArticle.written_by}</p>
-      <p data-cy='body' >{backyardArticle.body}</p>    
+    <div data-cy='article-container' className='backyard-container box-shadow'>
+      <h1 data-cy='title' className='article-title'>
+        {backyardArticle.title}
+      </h1>
+      <h2 data-cy='theme' className='article-author' >
+        {backyardArticle.theme}
+      </h2>
+      <p data-cy='author' className='article-category' >
+        Written By: {backyardArticle.written_by}
+      </p>
+      <p data-cy='date' className='article-date'>
+        {backyardArticle.date}
+      </p>
+      <p data-cy='body' className='article-body'>
+        {backyardArticle.body}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default BackyardArticle
-
-const styles = {
-  container: {
-    color: 'white',
-    margin: '20px 20%',
-    backgroundColor: '#202325',
-    padding: '25px',
-    borderRadius: 3,
-
-  }
-}
+export default BackyardArticle;
