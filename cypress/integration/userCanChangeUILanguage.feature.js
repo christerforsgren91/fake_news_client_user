4209cd94a4f33701eace63453e380c92ee611737
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-24 20:32:22 +0200

    adds tests

---
+++ cypress/integration/userCanChangeUILanguage.feature.js
@@ -0,0 +1,14 @@
+describe('user can change UI language', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+  
+  context('choosing Swedish language', () => {
+    it('is expected to translate UI into Swedish', () => {
+      cy.get('[data-cy=home-tab]').should('contain', 'Hem')
+      cy.get('[data-cy=backyard-tab]').should('contain', 'Bakgård')
+      cy.get('[data-cy=login-button]').should('contain', 'Logga In')
+      cy.get('[data-cy=category-button]').first().should('contain', 'Vetenskap')
+    });
+  })
+})
