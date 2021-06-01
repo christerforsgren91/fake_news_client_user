1fb0d471095f00764e9d4953d46900785bf3c6a4
lucas <laksteelhouse@gmail.com>
2021-06-01 22:28:00 +0200

    Tests passes locally

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 const PageNotFound = () => {
   return (
     <div>
-      <img data-cy='error-image' className='error-image' src='image/image.png' alt='error-message' />
+      <img
+        data-cy='error-image'
+        className='error-image'
+        src='image/image.png'
+        alt='error-message'
+      />
     </div>
-  );
-};
+  )
+}
 
-export default PageNotFound;
+export default PageNotFound
